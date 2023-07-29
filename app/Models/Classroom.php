55995bd2d003f28ce11590b238f4203513101f63
John Doe <engnoura2023@gmail.com>
2023-07-29 15:25:55 +0300

    new edit

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -38,7 +38,7 @@ class Classroom extends Model
 
     public static function deleteCoverImage($path)
     {
-        if(!$path || Storage::disk(Classroom::$disk)->exists($path)){
+        if(!$path || !Storage::disk(Classroom::$disk)->exists($path)){
             return ;
         }
         return Storage::disk(Classroom::$disk)->delete($path);
