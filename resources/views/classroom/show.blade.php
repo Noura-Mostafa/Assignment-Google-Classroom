5eb86984d54e5a51c0e4269f7f62d6b8b0c0daa7
John Doe <engnoura2023@gmail.com>
2023-08-05 19:40:20 +0300

    new edit

--- resources/views/classroom/show.blade.php
+++ resources/views/classroom/show.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.master')
+@extends('layouts.secondNav')
 
 @section('title' , 'Show')
 @section('content')
 
 <div class="container p-5">
-  
+
   <div class="header position-relative border rounded w-100">
     <img src="{{ asset('uploads/' . $classroom->cover_image_path)}}" alt="..." class="w-100 border rounded">
     <div class="content position-absolute bottom-0 start-0 p-5  text-white">
@@ -16,29 +16,33 @@
   <div class="row mt-3">
 
     <div class="col-md-3">
-      <div class="border rounded p-3 text-center">
+
+      <div class="border rounded p-3 text-center d-flex justify-content-between">
+        <h5 class="mt-2 text-start">Join Link:</h5>
+        <span id="textToCopy" style="display: none;">{{$invitation_link}}</span>
+        <button onclick="copyText()" class="btn btn-success"><i class="fas fa-copy"></i></button>
+      </div>
+
+      <div class="border rounded p-3 text-center mt-2">
         <h5 class="text-start">Class Code:</h5>
         <span class="text-success fs-2 ">{{$classroom->code}}</span>
       </div>
+
       <div class="border rounded p-3 text-center mt-2">
         <h5 class="text-start">Upcoming</h5>
         <h6 class="text-secondary">No work due soon</h6>
       </div>
+
     </div>
 
     <div class="col-md-9">
       <div class="border rounded p-3 mb-2">
-        <h5>invitation link :</h5>
-        <a class="text-secondary fs-6" href="{{$invitation_link}}">{{$invitation_link}}</a>
-      </div>
-
-      <div class="border rounded p-3 mb-2">
-        <a class="btn btn-outline-dark fs-6" href="{{route('classrooms.classworks.index' , $classroom->id)}}">Classworks</a>
       </div>
 
     </div>
 
   </div>
 </div>
+
 
 @endsection
--- resources/views/layouts/secondNav.blade.php
+++ resources/views/layouts/secondNav.blade.php
@@ -4,11 +4,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title }}</title>
+    <title>@yield('title' , config('app.name'))</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/main.css">
     <script src="https://kit.fontawesome.com/6cb271fdf5.js" crossorigin="anonymous"></script>
 
+    <script>
+        function copyText() {
+            const text = document.getElementById('textToCopy').innerText;
+
+            const tempInput = document.createElement('input');
+            tempInput.type = 'text';
+            tempInput.value = text;
+
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand('copy');
+            document.body.removeChild(tempInput);
+
+        }
+    </script>
 
 </head>
 
@@ -18,22 +33,42 @@
         <nav class="navbar navbar-expand-md bg-body-tertiary">
             <div class="container">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                   {{-- <li class="nav-item">
+                    {{--<li class="nav-item">
                         <a class="nav-link text-secondary fs-5" href="#" onclick="openNav()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                             </svg></a>
                     </li>--}}
-                    
                     <li class="nav-item">
                         <a class="navbar-brand text-secondary fs-4" href="{{route('classrooms.index')}}">
-                            <img src="{{ asset('imgs/googlelogo_clr_74x24px.svg') }}" alt="Logo" class="d-inline-block align-text-center">
+                            <img src="{{asset('imgs/googlelogo_clr_74x24px.svg')}}" alt="Logo" class="d-inline-block align-text-center">
                             Classroom</a>
                     </li>
                 </ul>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav  mb-2 mb-lg-0">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('classrooms.show' , $classroom->id)}}">Stream</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">People</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('classrooms.classworks.index' , $classroom->id)}}">Classwork</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Grade</a>
+                        </li>
+                    </ul>
+
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-secondary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -56,13 +91,17 @@
                 </div>
             </div>
         </nav>
+
+
     </header>
 
     <main>
-        {{ $slot }}
+        @yield('content')
     </main>
 
 
+
+    @stack('scripts')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
